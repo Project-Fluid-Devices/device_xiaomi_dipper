2a3fa9a03f4ee90bfea456c2a2442dc6c6b92873
ender1324 <ender1324@abv.bg>
2021-01-10 11:04:00 +0000

    dipper: Adapt to fluid

--- fluid_dipper.mk
+++ fluid_dipper.mk
@@ -6,12 +6,19 @@
 
 $(call inherit-product, device/xiaomi/dipper/device.mk)
 
-# Inherit some common PixelExperience stuff.
+# Inherit some common Fluid stuff.
 TARGET_BOOT_ANIMATION_RES := 1080
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+TARGET_INCLUDE_GAPPS := true
+#TARGET_GAPPS_ARCH := arm64
+TARGET_INCLUDE_GOOGLE_RECORDER := true
+TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_INCLUDE_MINIMAL_GAPPS := false
+TARGET_INCLUDE_AOSP_REPLACEMENTS := true
+$(call inherit-product, vendor/fluid/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_dipper
+PRODUCT_NAME := fluid_dipper
 PRODUCT_DEVICE := dipper
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := MI 8
