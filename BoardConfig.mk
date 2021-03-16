229ee3a1af21a93b4c80a1938cd45e0f6ecb88d6
ender1324 <ender1324@abv.bg>
2021-03-16 15:11:17 +0000

    dipper: Add support for automated DC Dimming
    Change-Id: I41be8162a55d0c8f27d5248d815438a740cad88d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -36,5 +36,8 @@ TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 # Recovery
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
 
+# Sepolicy
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
+
 # Inherit from the proprietary version
 -include vendor/xiaomi/dipper/BoardConfigVendor.mk
